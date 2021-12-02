97ae286a37428cc2b6f14408520edaf6aac75965
Artem Schirov <artemschirov@gmail.com>
2021-12-02 22:36:09 +0200

    feat: js parse cards.txt and render cards on page

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,10 @@ const textarea = document.getElementById('load-file__textarea_id');
 const cards = document.querySelector('.cards');
 const starredTemplate = document.querySelector('#starred-template').content;
 const cardTemplate = document.querySelector('#card-template').content;
+const cardExplanationTemplate = document.querySelector('#card-explanation').content;
+const cardSentenceTemplate = document.querySelector('#card-sentence').content;
+const cardListTemplate = document.querySelector('#card-list').content;
+// console.log(cardListTemplate);
 
 input.addEventListener('change', () => {
     const files = input.files;
@@ -17,26 +21,34 @@ input.addEventListener('change', () => {
     reader.onload = evt => {
         const file = evt.target.result;
         const lines = file.split(/\r\n|\n/);
-        // textarea.value = lines.join('\n');
         textarea.value = file;
         
         let cardElem = "";
+        let cardExplanation = "";
+        let cardSentence = "";
+        let cardList = "";
 
         for (let line = 0; line <= lines.length; line++) {
-            if (lines[line] === '') {  // empty line
-                console.log('==========')
+
+            if (!lines[line] || !lines[line].length) {  // empty line
                 cards.append(cardElem);
                 cardElem = cardTemplate.querySelector('.card').cloneNode(true);
+
             } else if (lines[line].slice(0, 4) === '    ') {  // sentence
-                console.log('    4')
-                let cardSentence = cardElem.querySelector('.card__sentence');
+                cardSentence = cardSentenceTemplate.querySelector('.card__sentence').cloneNode(true);
                 cardSentence.textContent = lines[line];
+                cardList.append(cardSentence);
+                if ((lines[line + 1].slice(0, 2) === '  ' && lines[line + 1].slice(2, 5) !== '  ') || !lines[line + 1] || !lines[line + 1].length) {
+                    cardElem.append(cardList);
+                }
+
             } else if (lines[line].slice(0, 2) === '  ') {  // explanation
-                console.log('  2')
-                let cardExplanation = cardElem.querySelector('.card__explanation');
+                cardList = cardListTemplate.querySelector('.card__list').cloneNode(true);
+                cardExplanation = cardExplanationTemplate.querySelector('.card__explanation').cloneNode(true);
                 cardExplanation.textContent = lines[line];
+                cardElem.append(cardExplanation);
+
             } else {  // word
-                console.log(lines[line]);
                 let cardTitle = cardElem.querySelector('.card__title');
                 cardTitle.textContent = lines[line];
             };
